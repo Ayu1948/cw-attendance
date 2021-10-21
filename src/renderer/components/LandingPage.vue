609c486c51bcae251c0181cc358fa7b27757ce3a
ZHTing <Ayu1948@163.com>
2021-10-21 09:58:14 +0800

    完成2.0更改

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -10,17 +10,36 @@
       <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
       <el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" />
     </el-table> -->
-    <el-switch
+    <el-upload
+      v-loading="loading"
+      element-loading-text="正在处理表格中"
+      element-loading-spinner="el-icon-loading"
+      element-loading-background="rgba(0, 0, 0, 0.8)"
+      class="upload-demo"
+      drag
+      action="/"
+      multiple
+      :limit="1"
+      :before-upload="upLoad"
+      >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      <div class="el-upload__tip" slot="tip">只能上传一个文件，且为表格</div>
+    </el-upload>
+    <!-- <el-switch
       v-model="value1"
       active-text="君尚"
       inactive-color="#13ce66"
       inactive-text="602"
     >
-    </el-switch>
+    </el-switch> -->
     <!-- <el-button @click="">下载</el-button> -->
-<a download="table导出Excel" id="excelOut" href="#" rel="external nofollow" >table导出Excel</a>
-    <table border="1" id="tableTo602">
-      <tr v-for="(item, index) in excelData602" :key="index">
+    <!-- <a class="downloadBtn" @click="getInit">开始处理</a> -->
+    <a class="downloadBtn" download="导出一部考勤表" id="excelOut1" href="#" rel="external nofollow" >导出一部考勤表</a>
+    <a class="downloadBtn" download="导出二部考勤表" id="excelOut2" href="#" rel="external nofollow" >导出二部考勤表</a>
+    <a class="downloadBtn" download="导出君尚考勤表" id="excelOut3" href="#" rel="external nofollow" >导出君尚考勤表</a>
+    <table id="tableToOne">
+      <tr v-for="(item, index) in excelDataOne" :key="index">
         <td
           width="38"
           v-for="(t, i) in item"
@@ -28,8 +47,7 @@
           :class="index >= 4 && index % 2 != 0 ? 'bw' : 'nw'"
           :colspan="index == 0 ? maxTd : 0"
           :rowspan="index == 0 ? 2 : 0"
-          :id="'602-' + index + '-' + i"
-          
+          :id="'One-' + index + '-' + i"
         >
           <fragment v-if="index >= 4 && index % 2 != 0">
             <fragment v-for="(e, i2) in t">{{ e }}</fragment>
@@ -41,16 +59,16 @@
         <th class="else-td" v-if="index==4">0-5min/次</th>
         <th class="else-td" v-if="index==4">5-30min/次</th>
         <th class="else-td" v-if="index==4">补打卡</th>
-        <td class="else-td" v-if="newto602[index]">{{newto602[index].name}}</td>
-        <td class="else-td" v-if="newto602[index]">{{newto602[index].orange}}</td>
-        <td class="else-td" v-if="newto602[index]">{{newto602[index].red}}</td>
-        <td class="else-td" v-if="newto602[index]">{{newto602[index].yellow}}</td>
+        <td class="else-td" v-if="newtoOne[index]">{{newtoOne[index].name}}</td>
+        <td class="else-td" v-if="newtoOne[index]">{{newtoOne[index].orange}}</td>
+        <td class="else-td" v-if="newtoOne[index]">{{newtoOne[index].red}}</td>
+        <td class="else-td" v-if="newtoOne[index]">{{newtoOne[index].yellow}}</td>
       </tr>
     </table>
     <!-- <table>
       <tr>
       </tr>
-      <tr v-for="item in newto602">
+      <tr v-for="item in newtoOne">
         <td>{{item.name}}</td>
         <td>{{item.orange}}</td>
         <td>{{item.red}}</td>
@@ -60,16 +78,16 @@
     <table border="1" id="tableToJs">
       <tr v-for="(item, index) in excelDataJS" :key="index">
         <td
+          width="38"
           v-for="(t, i) in item"
           :key="i"
           :class="index >= 4 && index % 2 != 0 ? 'bw' : 'nw'"
           :colspan="index == 0 ? maxTd : 0"
           :rowspan="index == 0 ? 2 : 0"
           :id="'js-' + index + '-' + i"
-          x:str
         >
           <fragment v-if="index >= 4 && index % 2 != 0">
-            <fragment v-for="(e, i2) in t"><br v-if="i2 != 0"/>{{ e }}</fragment>
+            <fragment v-for="(e, i2) in t">{{ e }}</fragment>
           </fragment>
           <fragment v-else>{{ t }}</fragment>
         </td>
@@ -84,10 +102,38 @@
         <td class="else-td" v-if="newtoJS[index]">{{newtoJS[index].yellow}}</td>
       </tr>
     </table>
+    <table border="1" id="tableToTwo">
+      <tr v-for="(item, index) in excelDataTwo" :key="index">
+        <td
+          width="38"
+          v-for="(t, i) in item"
+          :key="i"
+          :class="index >= 4 && index % 2 != 0 ? 'bw' : 'nw'"
+          :colspan="index == 0 ? maxTd : 0"
+          :rowspan="index == 0 ? 2 : 0"
+          :id="'Two-' + index + '-' + i"
+        >
+          <fragment v-if="index >= 4 && index % 2 != 0">
+            <fragment v-for="(e, i2) in t">{{ e }}</fragment>
+          </fragment>
+          <fragment v-else>{{ t }}</fragment>
+        </td>
+        
+        <th class="else-td" v-if="index==4">姓名</th>
+        <th class="else-td" v-if="index==4">0-5min/次</th>
+        <th class="else-td" v-if="index==4">5-30min/次</th>
+        <th class="else-td" v-if="index==4">补打卡</th>
+        <td class="else-td" v-if="newtoTwo[index]">{{newtoTwo[index].name}}</td>
+        <td class="else-td" v-if="newtoTwo[index]">{{newtoTwo[index].orange}}</td>
+        <td class="else-td" v-if="newtoTwo[index]">{{newtoTwo[index].red}}</td>
+        <td class="else-td" v-if="newtoTwo[index]">{{newtoTwo[index].yellow}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+const {shell} = require('electron').remote
 import xlsx from 'node-xlsx'
 // import fs from 'fs'
 // const ExcelJS = require('exceljs/dist/es5');
@@ -103,68 +149,64 @@ export default {
       errorMsg: '', // 错误信息内容
       excelTitle: {}, // excel标题
       excelData: [], // excel处理数据
-      excelData602: [], // excel处理数据
-      excelDataJS: [], // excel处理数据
-      newto602: null,
+      excelDataOne: [], // excel处理数据 一部
+      excelDataTwo: [], // excel处理数据 二部
+      excelDataJS: [], // excel处理数据 君尚
+      newtoOne: null,
+      newtoTwo: null,
       newtoJS: null,
-      maxTd: 0
+      maxTd: 0,
+      fileInfo: null,
+      loading: false,
+      isSix: true // 是否从第六行开始算
     }
   },
   created() {
-    this.getInit()
+    // this.getInit()
+    // this.openFileHandler()
   },
   mounted() {
-    this.maxTd = document.getElementById('tableTo602').rows[3].cells.length
-    setTimeout(() => {
-      this.newto602.forEach((item, index) => {
-        item.list.forEach((t, i) => {
-          if (t) {
-            // console.log(t, i, index)
-            if (t == 'yellow' || t == 'grey')
-            document.getElementById('602-' + index + '-' + i).style = `background-color: ${t}`
-            else
-            document.getElementById('602-' + index + '-' + i).style = `color: ${t}`
-          }
-        })
-      })
-      this.newtoJS.forEach((item, index) => {
-        item.list.forEach((t, i) => {
-          if (t) {
-            console.log(t, i, index)
-            if (t == 'yellow' || t == 'grey')
-            document.getElementById('js-' + index + '-' + i).style = `background-color: ${t}`
-            else
-            document.getElementById('js-' + index + '-' + i).style = `color: ${t}`
-          }
-        })
-      })
-      this.tableToExcel('tableTo602', '下载602')
-    }, 1000)
+    
   },
   methods: {
+    openFileHandler() {
+      const { shell } = require("electron").remote;
+      shell.showItemInFolder("C:");
+    },
+    upLoad(file) {
+      console.log(file)
+      this.fileInfo = file
+      if (file && file.name.indexOf('xls') !== -1) {
+        this.$message({
+          message: '上传成功，即将处理',
+          type: 'success'
+        });
+        this.loading = true
+        this.getInit();
+      } else {
+        this.$message.error('上传失败，请重试');
+      }
+    },
     afterInit() {
+      console.log(this.excelDataJS)
+      console.log(this.excelDataOne)
+      console.log(this.excelDataTwo)
       const that = this
-      this.newto602 = []
-      this.excelData602.forEach((item, index) => {
+      this.newtoOne = []
+      this.excelDataOne.forEach((item, index) => {
         if (index >= 4 && index % 2 !== 0) {
           let tmp = [],
             red = 0,
             orange = 0,
             grey = 0,
             yellow = 0
-          if (this.excelData602[index - 1][10] == '付丹丹') {
+          if (this.excelDataOne[index - 1][10] == '付丹丹') {
             item.forEach((t, i) => {
               tmp[i] = that.getClassFu(t, index, i)
               if (tmp[i] == 'red') red++
               else if (tmp[i] == 'orange') orange++
               else if (tmp[i] == 'grey') grey++
-              else if (tmp[i] == 'yellow') {
-                if (yellow < 3) yellow++
-                else {
-                  tmp[i] = 'red'
-                  red++
-                }
-              }
+              else if (tmp[i] == 'yellow') yellow++
             })
           } else {
             item.forEach((t, i) => {
@@ -172,18 +214,12 @@ export default {
               if (tmp[i] == 'red') red++
               else if (tmp[i] == 'orange') orange++
               else if (tmp[i] == 'grey') grey++
-              else if (tmp[i] == 'yellow') {
-                if (yellow < 3) yellow++
-                else {
-                  tmp[i] = 'red'
-                  red++
-                }
-              }
+              else if (tmp[i] == 'yellow') yellow++
             })
           }
 
-          that.newto602[index] = {
-            name: this.excelData602[index - 1][10],
+          that.newtoOne[index] = {
+            name: this.excelDataOne[index - 1][10],
             list: tmp,
             red,
             orange,
@@ -192,7 +228,35 @@ export default {
           }
         }
       })
-      console.log(this.newto602)
+      console.log(this.newtoOne)
+      this.newtoTwo = []
+      this.excelDataTwo.forEach((item, index) => {
+        if (index >= 4 && index % 2 !== 0) {
+          let tmp = [],
+            red = 0,
+            orange = 0,
+            grey = 0,
+            yellow = 0;
+          
+          item.forEach((t, i) => {
+            tmp[i] = that.getClass(t, index, i)
+            if (tmp[i] == 'red') red++
+            else if (tmp[i] == 'orange') orange++
+            else if (tmp[i] == 'grey') grey++
+            else if (tmp[i] == 'yellow') yellow++
+          })
+
+          that.newtoTwo[index] = {
+            name: this.excelDataTwo[index - 1][10],
+            list: tmp,
+            red,
+            orange,
+            grey,
+            yellow
+          }
+        }
+      })
+      console.log(this.newtoTwo)
       this.newtoJS = []
       this.excelDataJS.forEach((item, index) => {
         if (index >= 4 && index % 2 !== 0) {
@@ -207,11 +271,7 @@ export default {
             else if (tmp[i] == 'orange') orange++
             else if (tmp[i] == 'grey') grey++
             else if (tmp[i] == 'yellow') {
-              if (yellow < 3) yellow++
-              else {
-                tmp[i] = 'red'
-                red++
-              }
+              yellow++
             }
           })
           that.newtoJS[index] = {
@@ -225,11 +285,55 @@ export default {
         }
       })
       console.log(this.newtoJS)
+      this.maxTd = this.excelDataOne[3].length
+      setTimeout(() => {
+        this.newtoOne.forEach((item, index) => {
+          item.list.forEach((t, i) => {
+            if (t) {
+              // console.log(t, i, index)
+              if (t == 'yellow' || t == 'grey')
+              document.getElementById('One-' + index + '-' + i).style = `background-color: ${t}`
+              else
+              document.getElementById('One-' + index + '-' + i).style = `color: ${t}`
+            }
+          })
+        })
+        this.newtoTwo.forEach((item, index) => {
+          item.list.forEach((t, i) => {
+            if (t) {
+              // console.log(t, i, index)
+              if (t == 'yellow' || t == 'grey')
+              document.getElementById('Two-' + index + '-' + i).style = `background-color: ${t}`
+              else
+              document.getElementById('Two-' + index + '-' + i).style = `color: ${t}`
+            }
+          })
+        })
+        this.newtoJS.forEach((item, index) => {
+          item.list.forEach((t, i) => {
+            if (t) {
+              console.log(t, i, index)
+              if (t == 'yellow' || t == 'grey')
+              document.getElementById('js-' + index + '-' + i).style = `background-color: ${t}`
+              else
+              document.getElementById('js-' + index + '-' + i).style = `color: ${t}`
+            }
+          })
+        })
+        this.tableToExcel('tableToOne', '下载一部', 1)
+        this.tableToExcel('tableToTwo', '下载二部', 2)
+        this.tableToExcel('tableToJs', '下载JS', 3)
+        this.loading = false
+        this.$message({
+          message: '处理成功',
+          type: 'success'
+        });
+      }, 1000)
     },
     getClass(arr, row, col) {
       let flag = 0 // 0: 正常 '' 1：5分钟内 orange 2：迟到 red 3: 补卡 yellow -1: 无记录
       const l = arr.length
-      const maxTd = this.excelData602[3].length
+      const maxTd = this.excelDataOne[3].length
       switch (l) {
         case 0:
           flag = -1
@@ -265,7 +369,7 @@ export default {
     getClassFu(arr, row, col) {
       let flag = 0 // 0: 正常 '' 1：5分钟内 orange 2：迟到 red 3: 补卡 yellow -1: 无记录
       const l = arr.length
-      const maxTd = this.excelData602[3].length
+      const maxTd = this.excelDataOne[3].length
       switch (l) {
         case 0:
           flag = -1
@@ -304,6 +408,7 @@ export default {
         pm = arr[l]
       let tmp1 = am.split(':')
       if (!pm) pm = arr[l - 1]
+      // console.log(arr)
       let tmp2 = pm.split(':')
       let amLastTime = 9
       if (Number(tmp1[0]) >= 0 && Number(tmp1[0]) < 3) {
@@ -338,7 +443,7 @@ export default {
       const l = arr.length - 1
       let am = arr[0],
         pm = arr[l]
-      console.log(arr)
+      // console.log(arr)
       let tmp1 = am.split(':')
       if (!pm) pm = arr[l - 1]
       let tmp2 = pm.split(':')
@@ -408,6 +513,7 @@ export default {
         if (s) {
           const reg = /.{5}/g
           let rs = s.match(reg)
+          // console.log(s)
           rs.push(s.substring(rs.join('').length))
           newArr[i] = rs
         } else {
@@ -422,37 +528,43 @@ export default {
       //   .then(function() {
       //     const ws = workbook.getWorksheet(2)
       //     const row = ws.getRow(6)
-      //     console.log(row)
+      //     console.log(row) 
       //   })
-      const workSheetsFromFile = xlsx.parse(`${__dirname}/../assets/goal.xls`)
+      console.log(111)
+      const workSheetsFromFile = xlsx.parse(this.fileInfo.path)
       console.log(workSheetsFromFile)
       workSheetsFromFile.forEach((e) => {
         if (e.name == '考勤记录') {
           console.log(e.data)
-          this.excelData602 = e.data
+          this.excelDataOne = e.data
         }
       })
-      this.excelData602.forEach((e, i) => {
+      this.excelDataOne.forEach((e, i) => {
+        // console.log(e, i)
         if (i > 4 && i % 2 != 0) {
-          this.excelData602[i] = this.sliceArray(e)
+          this.excelDataOne[i] = this.sliceArray(e)
         }
       })
-      console.log(this.excelData602)
-      for (let i = this.excelData602.length - 1; i >= 0; i--) {
-        let e = this.excelData602[i]
-        const max = this.excelData602[3].length
+      console.log(this.excelDataOne)
+      for (let i = this.excelDataOne.length - 1; i >= 0; i--) {
+        let e = this.excelDataOne[i]
+        const max = this.excelDataOne[3].length
         if (e.length < max && i !== 0 && i !== 1) {
           e[max - 1] = ''
         }
-
-        if (e.indexOf('君尚') !== -1) {
-          // console.log()
-
-          this.excelDataJS.unshift(e, this.excelData602[i + 1])
-          this.excelData602.splice(i, 2)
+        console.log(e)
+        if (e.toString().indexOf('发行') !== -1) {
+          console.log(e)
+          this.excelDataJS.unshift(e, this.excelDataOne[i + 1])
+          this.excelDataOne.splice(i, 2)
+        } else if (e.toString().indexOf('二部') !== -1) {
+          console.log(e)
+          this.excelDataTwo.unshift(e, this.excelDataOne[i + 1])
+          this.excelDataOne.splice(i, 2)
         }
         if (i >= 0 && i <= 3) {
           this.excelDataJS.unshift(e)
+          this.excelDataTwo.unshift(e)
         }
       }
       this.afterInit()
@@ -479,14 +591,14 @@ export default {
           return c[p];
         });
     },
-    tableToExcel(tableid, sheetName) {
+    tableToExcel(tableid, sheetName, id) {
       var uri = 'data:application/vnd.ms-excel;base64,';
       var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel"'+
 		    'xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>'
 		    +'<x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets>'
 		    +'</x:ExcelWorkbook></xml><![endif]-->' +
         
-        '</head><body ><table class="tableTo602" x:str>' +
+        '</head><body ><table x:str>' +
         ' <style type="text/css">' +
         // 'table {' +
         //   'border-collapse: collapse;' +
@@ -550,8 +662,10 @@ export default {
         '</style>' +
         '{table}</table></body></html>';
       if (!tableid.nodeType) tableid = document.getElementById(tableid);
+      console.log(tableid)
       var ctx = {worksheet: sheetName || 'Worksheet', table: tableid.innerHTML};
-      document.getElementById("excelOut").href = uri + this.base64(this.format(template, ctx));
+
+      document.getElementById("excelOut" + id).href = uri + this.base64(this.format(template, ctx));
       // console.log(uri + this.base64(this.format(template, ctx)))
     }
   }
@@ -560,9 +674,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+.downloadBtn {
+  padding: 5px;
+  color: #000;
+  text-decoration: none;
+  display: inline-block;
+  margin-right: 5px;
+  border: 1px solid #000;
+  cursor: pointer;
+}
 table {
   border-collapse: collapse;
   table-layout: fixed;
+  border: 1px solid #000;
   tr {
     border-top: 1px solid #000;
     border-bottom: 1px solid #000;
@@ -590,7 +714,7 @@ table {
   }
   td {
     border: none;
-    width: 38px !important;
+    width: 38px;
     height: 100%;
     /* display: block; */
     min-height: 25px;
